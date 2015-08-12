9d15fab2c3e52446297e97db45dff3d25205d5a6
vinsanchen <vinsanchen@VinsanChendeMacBook.local>
2015-08-12 09:22:50 +0800

    1111

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DictUnifier
+//
+//  Created by Jiang Jiang on 3/7/10.
+//  Copyright Jjgod Jiang 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
